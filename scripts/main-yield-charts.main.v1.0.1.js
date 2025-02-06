ba0dc9dbf596fbae2e963f9b6d4e6320b69a5765
MatiasHumud <matiashumud@gmail.com>
2025-02-06 11:10:57 -0300

    Add helpers to each script

--- scripts/main-yield-charts.main.v1.0.1.js
+++ scripts/main-yield-charts.main.v1.0.1.js
@@ -1,4 +1,59 @@
-const fetchDataApi = require('./helpers/fetch-data-api');
+const nationalStocksYields = {
+  "yields": [
+    {
+        "Perfil": 6.0,
+        "rentabilidadMesCLP": 0.06450688699493035,
+        "mes": "2024-09"
+    },
+    {
+        "Perfil": 6.0,
+        "rentabilidadMesCLP": 0.0088794,
+        "mes": "2024-10"
+    },
+    {
+        "Perfil": 6.0,
+        "rentabilidadMesCLP": 0.0053629,
+        "mes": "2024-11"
+    },
+    {
+        "Perfil": 6.0,
+        "rentabilidadMesCLP": 0.0360834,
+        "mes": "2024-12"
+    }
+  ]
+}
+
+const BASE_URL = 'https://91o7sqo7s3.execute-api.us-east-1.amazonaws.com/prod';
+
+const buildQueryString = (queryObject) => Object.entries(queryObject)
+  .filter(([_, value]) => !!value)
+  .map(([key, value]) => `${key}=${value}`)
+  .join('&');
+
+const buildPathBy = (portfolioType) => ({
+  harryIpsa: 'harryIpsaYields',
+  wallet: 'walletYields',
+  smart: 'recommendationYields',
+}[portfolioType]);
+
+const fetchDataApi = ({
+  portfolioType,
+  year,
+  month,
+  risk,
+  typeUniverse
+}) => {
+  if (portfolioType === 'harryIpsa') return nationalStocksYields; // TODO: fetch this from the API
+
+  const url = `${BASE_URL}/${buildPathBy(portfolioType)}?${buildQueryString({ year, month, risk, typeUniverse })}`
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  return fetch(url, requestOptions).then(response => response.json())
+}
 
 const MOTNHS = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago','Sept', 'Oct', 'Nov', 'Dic']
 
